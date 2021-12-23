6cebd95aac8cb7ea871a987b798a034a1a940683
tahminaislamshammee <tahminaislamshammee@gmail.com>
2021-12-23 17:39:56 +0600

    image path changed

---
+++ src/Components/back/data.js
@@ -0,0 +1,42 @@
+const data = {
+    productItems:[
+        {
+            id: '1',
+            name: 'headphone',
+            price: 999,
+            image: 'pics/laptop.jpg',
+        },
+        {
+            id: '2',
+            name: 'headphone 2',
+            price: 9999,
+            image: 'pics/laptop2.jpg'
+        },
+        {
+            id: '3',
+            name: 'headphone 3',
+            price: 799,
+            image: 'pics/laptop.jpg'
+        },
+        {
+            id: '4',
+            name: 'headphone 4',
+            price: 799,
+            image: 'pics/laptop2.jpg'
+        },
+        {
+            id: '5',
+            name: 'headphone 4',
+            price: 799,
+            image: 'pics/laptop.jpg'
+        },
+        {
+            id: '6',
+            name: 'headphone 4',
+            price: 799,
+            image: 'pics/laptop2.jpg'
+        },
+        
+    ]
+}
+export default data;
